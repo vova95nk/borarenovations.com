085a06ac25235041409e12d7da334333fabbac4d
vladimir <vladimir.n@apptica.com>
2024-10-04 23:29:57 +0300

    BR-1. Pre-release updates.

--- resources/views/main/parts/google-map.blade.php
+++ resources/views/main/parts/google-map.blade.php
@@ -1,4 +1,9 @@
 <div class="google-map-container">
+    <div class="map_text">
+        <p>
+            “Your local cabinet renovation experts. </br> Contact us to see how we can transform your kitchen.”
+        </p>
+    </div>
     <iframe src="https://www.google.com/maps/d/embed?mid=1S1SNYdjtzC2iyy_r0Gfdrzn9fToyFt0&ehbc=2E312F&noprof=1"
             height="450" style="border:0;" allowfullscreen="" loading="lazy"
             referrerpolicy="no-referrer-when-downgrade" class="google-map">
