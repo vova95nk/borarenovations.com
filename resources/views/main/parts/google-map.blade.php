c4b749b62fba687cc7cfb8f179064ca1ac84538e
vladimir <vladimir.n@apptica.com>
2024-10-07 23:50:09 +0300

    BR-1. Pre-release updates.

--- resources/views/main/parts/google-map.blade.php
+++ resources/views/main/parts/google-map.blade.php
@@ -1,7 +1,7 @@
 <div class="google-map-container">
     <div class="map_text">
         <p>
-            “Your local cabinet renovation experts. </br> Contact us to see how we can transform your kitchen.”
+            Your local cabinet renovation experts. </br><a href="#" style="text-decoration: none; color: white; font-weight: bold">Contact us</a> to see how we can transform your kitchen.
         </p>
     </div>
     <iframe src="https://www.google.com/maps/d/embed?mid=1S1SNYdjtzC2iyy_r0Gfdrzn9fToyFt0&ehbc=2E312F&noprof=1"
